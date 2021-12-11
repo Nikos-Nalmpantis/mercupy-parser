59c0b3268e26988ce9e1e41eb53f7379273746a8
nikosNalmpantis <nikosnalmpantis@outlook.com>
2021-12-11 08:41:24 +0200

    feat: replace md with rst

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
+import os
+import sys
+
 from setuptools import setup
 
-setup(name='mercupy_parser',
-      version='0.1.1',
-      description='Python wrapper for mercury-parser',
-      url='https://github.com/nikosNalmpantis/mercupy-parser',
-      author='Nikos Nalmpantis',
-      author_email='nikosnalmpantis@outlook.com',
-      license='GNU General Public License v3.0',
-      packages=['mercupy_parser'],
-      install_requires=[
-          'httpx',
-          'validators',
-      ],
-      zip_safe=False)
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as f:
+    long_description = f.read()
+
+
+setup(
+    name='mercupy_parser',
+    version='0.1.2',
+    description='Python wrapper for mercury-parser',
+    long_description=long_description,
+    url='https://github.com/nikosNalmpantis/mercupy-parser',
+    author='Nikos Nalmpantis',
+    author_email='nikosnalmpantis@outlook.com',
+    license='GNU General Public License v3.0',
+    packages=['mercupy_parser'],
+    install_requires=[
+      'httpx',
+      'validators',
+    ],
+    zip_safe=False
+)
