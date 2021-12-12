c586ad2a9e7893b673fd2a50a77d0e0d03168d20
nikosNalmpantis <nikosnalmpantis@outlook.com>
2021-12-12 19:53:34 +0200

    fix: better code format

--- mercupy_parser/__init__.py
+++ mercupy_parser/__init__.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
+import time
 from functools import reduce
-from time import perf_counter
 from typing import List, Tuple, Union, Optional
 from urllib.parse import urljoin as join
 
@@ -10,11 +10,14 @@ from httpx import Response
 
 
 def urljoin(*terms: str) -> str:
+    """ Correctly adds terms to a url."""
     return reduce(join, terms)
 
 
 class Mercupy:
-    def __init__(self, api_endpoint: Optional[str] = None, verbose: bool = False):
+    def __init__(self,
+                 api_endpoint: Optional[str] = None,
+                 verbose: bool = False) -> None:
         api_endpoint = api_endpoint or os.environ.get("API_ENDPOINT", "http://0.0.0.0:4000")
         self.api_endpoint = urljoin(api_endpoint, "parser")
         self.verbose = verbose
@@ -34,15 +37,16 @@ class Mercupy:
         return responses
 
     async def _parser(self,
-                     urls: Union[str, List[str]],
-                     headers: Optional[str] = None,
-                     content_type: Optional[str] = None,
-                     timeout: int = 20) -> Tuple[Response]:
+                      urls: List[str],
+                      headers: Optional[str] = None,
+                      content_type: Optional[str] = None,
+                      timeout: int = 20) -> Tuple[Response]:
         """ Convenience method to parse multiple urls """
         params = {"headers": headers, "contentType": content_type}
+        # Keep only values that are not None
         params = {key: value for key, value in params.items() if value}
 
-        start = perf_counter()
+        start = time.perf_counter()
 
         async with httpx.AsyncClient() as client:
             tasks = (client.get(self.api_endpoint,
@@ -51,8 +55,9 @@ class Mercupy:
                      for url in urls)
             responses = await asyncio.gather(*tasks)
 
-        end = perf_counter()
+        end = time.perf_counter()
         if self.verbose is True:
             print(f"Response time: {end - start:.2f} sec")
 
         return responses
+
